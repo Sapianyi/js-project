6d2595fc2fb212f275ce45b4ca5293a4e555c4e5
Mikhailo Sapianyi <sapianyi@gmail.com>
2023-11-30 22:25:11 +0200

    first comit js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+    // ++incr;
+    // --decr;
+    // console.log(++incr, --decr);
+    // console.log(5%2);
+
+    console.log((2 + 2) * 2 != 8);
+
+    // const isChecked = false,
+    //       isClose = false;
+          
+    // console.log(isChecked || !isClose);
